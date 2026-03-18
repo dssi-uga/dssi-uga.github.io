b2e8d7b02ce5e83a5b59e59c539b384b8b8f1394
dssi-uga <labdrsun@gmail.com>
2026-03-18 03:21:51 +0000

    Deploying to gh-pages from @ dssi-uga/dssi-uga.github.io@de22f6ac882cc33b82014288e06afc17faf1cb12 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-about",
-          title: "about",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/";
-          },
-        },{id: "nav-people",
+  },{id: "nav-people",
           title: "people",
           description: "members of the lab or group",
           section: "Navigation",
