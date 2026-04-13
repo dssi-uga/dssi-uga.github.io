5a358ae5ac00bc0f77a88912b9dcaace2e04e725
nehalsiv-del <nehalyasivakumar@gmail.com>
2026-04-13 17:51:45 +0000

    Deploying to gh-pages from @ dssi-uga/dssi-uga.github.io@ff70275d628035f0adc281e70d7887759dbf9c84 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,7 +9,14 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-projects",
+  },{id: "nav-people",
+          title: "People",
+          description: "members of the lab or group",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/people/";
+          },
+        },{id: "nav-projects",
           title: "Projects",
           description: "A growing collection of your cool projects.",
           section: "Navigation",
