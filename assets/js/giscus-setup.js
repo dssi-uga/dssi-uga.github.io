6c4eeddb9158b8c4f6e81d7a54d9a24890548b16
merit-uga <labdrsun@gmail.com>
2026-07-14 20:33:57 +0000

    Deploying to gh-pages from @ merit-uga/merit-uga.github.io@59db6c845e08de17b6c2e53479b10578424272de 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "dssi-uga/dssi-uga.github.io",
+    "data-repo": "merit-uga/merit-uga.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
